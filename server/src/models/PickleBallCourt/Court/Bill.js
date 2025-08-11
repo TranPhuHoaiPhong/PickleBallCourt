67f86e2705c086e4fcd61e7d83a01bcc9c689ebf
AnKhang <minecraftcopyright1302@gmail.com>
2025-08-11 22:11:31 +0700

    fdfsfsdf

--- server/src/models/PickleBallCourt/Court/Bill.js
+++ server/src/models/PickleBallCourt/Court/Bill.js
@@ -1,22 +1,28 @@
 const mongoose = require("mongoose");
 
-const bookingHistorySchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
+const bookingHistorySchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    court: {
+      courtId: { type: mongoose.Schema.Types.ObjectId, ref: "Court" },
+    },
+    location: {
+      name: { type: String, required: true },
+      address: { type: String, required: true },
+      locationId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "CourtLocation",
+      },
+    },
+    startTime: { type: Date, required: true },
+    endTime: { type: Date, required: true },
+    totalPrice: { type: Number, required: true },
+    status: { type: String, enum: ["booked", "cancelled"], default: "booked" },
   },
-  court: {
-    courtId: { type: mongoose.Schema.Types.ObjectId, ref: 'Court' }
-  },
-  location: {
-    name: { type: String, required: true },
-    address: { type: String, required: true },
-    locationId: { type: mongoose.Schema.Types.ObjectId, ref: 'CourtLocation' }
-  },
-  startTime: { type: Date, required: true },
-  endTime: { type: Date, required: true },
-  totalPrice: { type: Number, required: true },
-  status: { type: String, enum: ['booked', 'cancelled'], default: 'booked' }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("BookingHistory", bookingHistorySchema);
